55fc2aec67858f177ed1a1e0f0412b4afb87938c
Peter Ralston <peternralston@gmail.com>
2022-08-23 09:28:30 -0500

    added some php

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,11 +1,9 @@
-This is our completely amazing custom theme.
-Last night we added a local version of this site to my home laptop
-But 'local' the wordpress app (localwp) could not see the cloned version from GitHub
-even though it was in the same folder. 
-So, I created a zip folder of the site to my desktop and drag and dropped the folder into the local app 
-Then, I closed the app and deleted the folder. 
-After it was deleted I did a git clone into the same folder. This way both the app and github were connected to the changes in the site!
-Pesto! I did it. 
+</php
+  function myFirstFunction() {
+    echo 2+2;
+  }
 
-Only now I am having trouble loggin into GitHub when doing a git push to the repository. 
-Lets see if I can git push these changes!
+  myFirstFunction();
+  myFirstFunction();
+  
+?>
