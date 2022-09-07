f571a80319c686b8beeffa7d13abb852fa08fe86
Peter Ralston <peternralston@gmail.com>
2022-09-07 11:01:37 -0500

    added images via php

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function university_files() {
-    wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css')); // See video 11 @ 9:25 for more information on how to do this
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
