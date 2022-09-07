74f3462f76945e5e3f70ddb01830a24864da9fa8
Peter Ralston <peternralston@gmail.com>
2022-09-07 10:26:18 -0500

    icons successfully added

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function university_files() {
-    wp_enqueue_style('font-awesome', 'maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css')); // See video 11 @ 9:25 for more information on how to do this
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
